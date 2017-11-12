e6751fbace9aa8ebfedcfc106dfae2623c811dda
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2017-11-12 19:43:18 +0000

    feat: created bookself component

---
+++ src/BookShelf.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Book from './Book'
+
+class BookShelf extends Component {
+
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    shelfInfo: PropTypes.object.isRequired
+  }
+
+  render() {
+
+    const { books, shelfInfo } = this.props
+
+    return(
+
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{shelfInfo.title}</h2>
+
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.filter( (book) => book.shelf === shelfInfo.value )
+              .map((book) => (
+
+              <li key={book.id}>
+                <Book
+                  bookInfo={book}
+                />
+              </li>
+
+            ))}
+          </ol>
+        </div>
+      </div>
+
+    )
+
+  }
+
+}
+
+export default BookShelf
