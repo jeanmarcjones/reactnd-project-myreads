246021c9a023990289a96931aa19e618ae260814
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2017-11-16 20:05:10 +0000

    refactor: Moved html and state into ListsBooks to stream line app.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import React, { Component } from 'react'
-import { Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import SearchBooks from './SearchBooks'
-import BookShelf from './BookShelf'
+import ListsBooks from './ListsBooks'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends Component {
 
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
     books: [],
-    shelves: [
-      {
-        value: 'currentlyReading',
-        title: 'Currently Reading'
-      },
-      {
-        value: 'wantToRead',
-        title: 'Want To Read'
-      },
-      {
-        value: 'read',
-        title: 'Read'
-      }
-    ]
   }
 
   componentDidMount() {
@@ -55,47 +35,35 @@ class BooksApp extends Component {
 
   render() {
 
-    const { books, shelves } = this.state
+    const { books, searchResults } = this.state
 
     return (
 
       <div className="app">
 
         <Route exact path="/" render={() => (
-            <div className="list-books">
-              <div className="list-books-title">
-                <h1>MyReads</h1>
-              </div>
 
-              <div className="list-books-content">
-                {shelves.map((shelf) => (
-
-                  <BookShelf
-                    key={shelf.value}
-                    books={books.filter((book) => book.shelf === shelf.value)}
-                    shelfInfo={shelf}
-                    onUpdateShelf={(book, shelf) => {
-                      this.updateBook(book, shelf)
-                    }}
-                  />
-
-                ))}
-              </div>
-
-              <div className="open-search">
-                <Link to="/search">Add a book</Link>
-              </div>
-            </div>
-          )}
-        />
-
-        <Route path="/search" render={() => (
-            <SearchBooks
+            <ListsBooks
               books={books}
               onUpdateShelf={(book, shelf) => {
                 this.updateBook(book, shelf)
               }}
+              onSearch={(query) => {
+                this.searchBooks(query)
+              }}
             />
+
+          )}
+        />
+
+        <Route path="/search" render={() => (
+
+            <SearchBooks
+              onUpdateShelf={(book, shelf) => {
+                this.updateBook(book, shelf)
+              }}
+            />
+
           )}
         />
 
---
+++ src/ListsBooks.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import Book from './Book'
+
+class BookShelf extends Component {
+
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    onUpdateShelf: PropTypes.func.isRequired
+  }
+
+  state = {
+    shelves: [
+      {
+        value: 'currentlyReading',
+        title: 'Currently Reading'
+      },
+      {
+        value: 'wantToRead',
+        title: 'Want To Read'
+      },
+      {
+        value: 'read',
+        title: 'Read'
+      }
+    ],
+  }
+
+  handleUpdate = (book, shelf) => {
+
+    if(this.props.onUpdateShelf) {
+      this.props.onUpdateShelf(book, shelf)
+    }
+
+  }
+
+  render() {
+
+    const { books } = this.props
+    const { shelves } = this.state
+
+    return(
+
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+
+        <div className="list-books-content">
+          {shelves.map((shelf) => (
+
+          <div key={shelf.value} className="bookshelf">
+              <h2 className="bookshelf-title">{shelf.title}</h2>
+
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {books.filter((book) => book.shelf === shelf.value).map((book) => (
+
+                    <li key={book.id}>
+                      <Book
+                        bookInfo={book}
+                        onUpdateShelf={this.handleUpdate}
+                      />
+                    </li>
+
+                  ))}
+                </ol>
+              </div>
+            </div>
+
+          ))}
+        </div>
+
+        <div className="open-search">
+          <Link to="/search">Add a book</Link>
+        </div>
+      </div>
+
+    )
+
+  }
+
+}
+
+export default BookShelf
