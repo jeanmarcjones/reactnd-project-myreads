b621f9d25527dda9f6955bc50f93f1cd0a718654
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2017-11-28 14:03:41 +0000

    refactor: Made book a function stateless componet as it has no state

--- src/Book.js
+++ src/Book.js
@@ -1,65 +1,57 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 
-class Book extends Component {
-  static protoTypes = {
-    bookInfo: PropTypes.array.isRequired,
-    onUpdateShelf: PropTypes.func.isRequired
-  };
+function Book (props)  {
 
-  handleChange = e => {
+  const handleChange = e => {
     e.preventDefault();
 
-    if (this.props.onUpdateShelf) {
-      this.props.onUpdateShelf(this.props.bookInfo, e.target.value);
+    if (props.onUpdateShelf) {
+      props.onUpdateShelf(props.bookInfo, e.target.value);
     }
   };
 
-  render() {
-    const { bookInfo } = this.props;
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{
+            width: 128,
+            height: 188,
+            backgroundImage: `url(http://books.google.com/books/content?id=${
+              props.bookInfo.id
+            }&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api)`
+          }}
+        />
 
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div
-            className="book-cover"
-            style={{
-              width: 128,
-              height: 188,
-              backgroundImage: `url(http://books.google.com/books/content?id=${
-                bookInfo.id
-              }&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api)`
-            }}
-          />
+        <div className="book-shelf-changer">
+          <select value={props.bookInfo.shelf} onChange={e => handleChange(e)}>
+            <option value="none" disabled>
+              Move to...
+            </option>
 
-          <div className="book-shelf-changer">
-            <select value={bookInfo.shelf} onChange={e => this.handleChange(e)}>
-              <option value="none" disabled>
-                Move to...
-              </option>
+            <option value="currentlyReading">Currently Reading</option>
 
-              <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
 
-              <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
 
-              <option value="read">Read</option>
-
-              <option value="none">None</option>
-            </select>
-          </div>
-        </div>
-
-        <div className="book-title">{bookInfo.title}</div>
-
-        <div className="book-authors">
-          {bookInfo.authors && (bookInfo.authors.map(
-            (author, index) =>
-              `${author}${bookInfo.authors[index + 1] ? ", " : ""}`
-          ))}
+            <option value="none">None</option>
+          </select>
         </div>
       </div>
-    );
-  }
+
+      <div className="book-title">{props.bookInfo.title}</div>
+
+      <div className="book-authors">
+        {props.bookInfo.authors &&
+        props.bookInfo.authors.map(
+            (author, index) =>
+              `${author}${props.bookInfo.authors[index + 1] ? ", " : ""}`
+          )}
+      </div>
+    </div>
+  );
 }
 
 export default Book;
