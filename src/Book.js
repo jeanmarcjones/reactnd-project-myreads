3b95306a39bf1c9bddb07c90e177186ba7782897
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-01-21 15:39:25 +0000

    refactor: made props an object
    Also added comment plus refactored code, to make clear there is no key
    in map over authors.

--- src/Book.js
+++ src/Book.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-function Book (props)  {
-
+function Book({ bookInfo, onUpdateShelf }) {
   const handleChange = e => {
     e.preventDefault();
 
-    if (props.onUpdateShelf) {
-      props.onUpdateShelf(props.bookInfo, e.target.value);
+    if (onUpdateShelf) {
+      onUpdateShelf(bookInfo, e.target.value);
     }
   };
 
@@ -19,13 +18,13 @@ function Book (props)  {
             width: 128,
             height: 188,
             backgroundImage: `url(http://books.google.com/books/content?id=${
-              props.bookInfo.id
+              bookInfo.id
             }&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api)`
           }}
         />
 
         <div className="book-shelf-changer">
-          <select value={props.bookInfo.shelf} onChange={e => handleChange(e)}>
+          <select value={bookInfo.shelf} onChange={e => handleChange(e)}>
             <option value="none" disabled>
               Move to...
             </option>
@@ -41,13 +40,13 @@ function Book (props)  {
         </div>
       </div>
 
-      <div className="book-title">{props.bookInfo.title}</div>
+      <div className="book-title">{bookInfo.title}</div>
 
       <div className="book-authors">
-        {props.bookInfo.authors &&
-        props.bookInfo.authors.map(
-            (author, index) =>
-              `${author}${props.bookInfo.authors[index + 1] ? ", " : ""}`
+        {bookInfo.authors &&
+          bookInfo.authors.map((author, index) =>
+            // Adds comma if not last item in array
+            `${author}${bookInfo.authors.length !== (index + 1) ? ", " : ""}`
           )}
       </div>
     </div>
